fc1126fb86391a40220cdc47e21d23e01228fce6
Michael Dunn <dunnmj42@gmail.com>
2021-02-03 08:36:29 -0600

    less/equal

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -10,7 +10,7 @@ router.get('/', rejectUnauthenticated, (req, res) => {
   console.log('req.user:', req.user);
 
   pool
-    .query(`SELECT * FROM "secret" WHERE "secrecy_level" < ${req.user.clearance_level};`)
+    .query(`SELECT * FROM "secret" WHERE "secrecy_level" <= ${req.user.clearance_level};`)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
